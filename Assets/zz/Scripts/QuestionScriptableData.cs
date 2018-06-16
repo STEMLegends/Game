5813ad7066f18182190fe0bc62729f3b2ed57f6a
Eldir <lothvorn@gmail.com>
2018-06-16 20:55:44 +0100

    Added simple questions puzzle skeleton

---
+++ Assets/zz/Scripts/QuestionScriptableData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "__NewQuestion__", menuName = "LoS/DataGenerators/Question", order = 1)]
+public class QuestionScriptableData : ScriptableObject {
+
+    public string m_question;
+    public string[] m_answer;
+
+    public int m_idRightAnswer;
+}
---
+++ Assets/zz/Scripts/QuestionsListScriptableData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "__NewQuestionList__", menuName = "LoS/DataGenerators/QuestionsList", order = 1)]
+    public class QuestionsListScriptableData : ScriptableObject
+{
+
+    public QuestionScriptableData[] m_questionsList;
+}
