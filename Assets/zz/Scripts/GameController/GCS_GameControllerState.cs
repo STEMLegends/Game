e4c79428fce1b8fc8f733011f9015692f46b278a
Eldir <lothvorn@gmail.com>
2018-06-23 23:17:07 +0100

    GameController linked to MinigameController

--- Assets/zz/Scripts/GameController/GCS_GameControllerState.cs
+++ Assets/zz/Scripts/GameController/GCS_GameControllerState.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GCS_GameControllerState : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class GCS_GameControllerState : StateGeneric<GameController>
+{
 }
--- Assets/zz/Scripts/GameController/GCS_PlayingMinigame.cs
+++ Assets/zz/Scripts/GameController/GCS_PlayingMinigame.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GCS_PlayingMinigame : StateGeneric<GameController> {
+public class GCS_PlayingMinigame : GCS_GameControllerState
+{
 
 
     public override void Enter()
-{
-        m_target.m_references.m_userControl.enabled = true;
-}
+    {
+        
+    }
 }
