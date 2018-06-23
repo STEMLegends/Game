3991de66596231d2c29af4b24a8575c89b92dc76
Eldir <lothvorn@gmail.com>
2018-06-23 19:44:31 +0100

    Added MainMenu scene and some minor changes into SampleScene

---
+++ Assets/zz/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void LSTR_Play() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LSTR_Credits() { }
+
+    public void LSTR_SwitchDifficulty ()
+    {
+
+    }
+
+    public void LSTR_GoToDifficulty()
+    {
+        m_references.m_mainMenu.SetActive(false);
+        m_references.m_difficultyMenu.SetActive(true);
+    }
+
+    public void LSTR_SetEasy () { }
+
+    public void LSTR_SetHard() { }
+
+    public void LSTR_BackToMain() {
+        m_references.m_mainMenu.SetActive(true);
+        m_references.m_difficultyMenu.SetActive(false);
+    }
+
+
+    private void Awake()
+    {
+        string difficulty = PlayerPrefs.GetString("Difficulty");
+
+        Debug.Log("----");
+    }
+    private void Start()
+    {
+        LSTR_BackToMain();
+    }
+
+    [SerializeField]
+    MainMenuControllerReferences m_references;
+
+    [System.Serializable]
+    public class MainMenuControllerReferences
+    {
+        public GameObject m_mainMenu;
+        public GameObject m_difficultyMenu;
+
+        public Text m_difficultyText;
+    }
+}
