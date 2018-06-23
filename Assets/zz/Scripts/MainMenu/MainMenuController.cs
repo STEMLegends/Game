e9c94f61a40910ea8937c943b526c752e011b5ec
Eldir <lothvorn@gmail.com>
2018-06-23 20:43:53 +0100

    Set difficulty menu changed to switch difficulty button

--- Assets/zz/Scripts/MainMenu/MainMenuController.cs
+++ Assets/zz/Scripts/MainMenu/MainMenuController.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenuController : MonoBehaviour {
+public class MainMenuController : MonoBehaviour
+{
 
-	public void LSTR_Play() {
+
+    #region LISTENERS
+    public void LSTR_Play()
+    {
         SceneManager.LoadScene("SampleScene");
     }
 
     public void LSTR_Credits() { }
 
-    public void LSTR_SwitchDifficulty ()
+    public void LSTR_SwitchDifficulty()
     {
+        string difficulty = PlayerPrefs.GetString("Difficulty");
+        if (difficulty == "Easy")
+            difficulty = "Hard";
+        else
+            difficulty = "Easy";
 
+        m_references.m_difficultyText.text = "Current difficulty " + difficulty;
+        PlayerPrefs.SetString("Difficulty", difficulty);
     }
 
     public void LSTR_GoToDifficulty()
@@ -23,24 +34,32 @@ public class MainMenuController : MonoBehaviour {
         m_references.m_difficultyMenu.SetActive(true);
     }
 
-    public void LSTR_SetEasy () { }
+    public void LSTR_SetEasy() { }
 
     public void LSTR_SetHard() { }
 
-    public void LSTR_BackToMain() {
+    public void LSTR_BackToMain()
+    {
         m_references.m_mainMenu.SetActive(true);
         m_references.m_difficultyMenu.SetActive(false);
     }
+    #endregion
 
 
-    private void Awake()
-    {
-        string difficulty = PlayerPrefs.GetString("Difficulty");
 
-        Debug.Log("----");
-    }
     private void Start()
     {
+        string difficulty = PlayerPrefs.GetString("Difficulty");
+        if (difficulty == "")
+        {
+            PlayerPrefs.SetString("Difficulty", "Easy");
+            difficulty = "Easy";
+        }
+
+        m_references.m_difficultyText.text = "Current difficulty " + difficulty;
+
+        Debug.Log("----");
+
         LSTR_BackToMain();
     }
 
