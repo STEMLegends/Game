e9d635c892bc3e176684873b08558065d576c439
Eldir <lothvorn@gmail.com>
2018-06-24 20:55:39 +0100

    Added skeleton for Quarantine mingame

--- Assets/zz/Scripts/Minigames/Rules/MinigameRulesController.cs
+++ Assets/zz/Scripts/Minigames/Rules/MinigameRulesController.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class MinigameRulesController : MonoBehaviour {
 
-
+    public void LSTR_CloseDescriptionAndStart()
+    {
+        m_gameContainer.SetActive(true);
+        m_descriptionContainer.SetActive(false);
+    }
     public virtual void Initialize (MinigameController whoCalled)
     {
         m_caller = whoCalled;
+        gameObject.SetActive(true);
+        m_gameContainer.SetActive(false);
+        m_descriptionContainer.SetActive(true);
+    }
+
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
     }
     public MinigameController m_caller;
+
+    public GameObject m_descriptionContainer;
+    public GameObject m_gameContainer;
 }
---
+++ Assets/zz/Scripts/Minigames/Rules/QuarantineRules.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuarantineRules : MonoBehaviour, IMinigameable
+{
+    public void StartMinigame(MinigameController caller)
+    {
+        m_quarantineController.Initialize(caller);
+    }
+
+
+    private void Awake()
+    {
+        m_quarantineController = FindObjectOfType<QuarantineController>();
+    }
+
+    QuarantineController m_quarantineController;
+}
