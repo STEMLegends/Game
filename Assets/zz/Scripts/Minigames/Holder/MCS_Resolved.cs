0517b5f4c7b6163cbe595eab66a24ba020b80d9b
Eldir <lothvorn@gmail.com>
2018-07-02 22:09:11 +0100

    Fix a null pointer exception when stoping particle effects and removes sprites for type 0 & type 3

--- Assets/zz/Scripts/Minigames/Holder/MCS_Resolved.cs
+++ Assets/zz/Scripts/Minigames/Holder/MCS_Resolved.cs
@@ -11,7 +11,9 @@ public class MCS_Resolved : MCS_MinigameControllerState
         m_target.m_references.m_mainSprite.sprite = m_target.m_stencils.m_resolvedSprite;
         m_target.m_gameController.SM_GoToPlayingScroller();
         m_target.m_references.m_collider.enabled = false;
-        m_target.m_references.m_particles.Stop();
-        m_target.m_references.m_particlesResolved.Play();
+        if (m_target.m_references.m_particles != null)
+            m_target.m_references.m_particles.Stop();
+        if (m_target.m_references.m_particlesResolved != null)
+            m_target.m_references.m_particlesResolved.Play();
     }
 }
