96ad5d7286307b685151d9d661a031c7a8752d19
sam-ji <armunzakor@gmail.com>
2018-10-14 04:50:31 +0200

    Add floating enemies

---
+++ Assets/zzMyAssets/Catfish/SinusoidalYMovement.cs
@@ -0,0 +1,20 @@
+﻿using Gamekit2D;
+using UnityEngine;
+
+public class SinusoidalYMovement : MonoBehaviour {
+
+    protected CharacterController2D m_CharacterController2D;
+    public float scale = 5;
+
+    void Start()
+    {
+        m_CharacterController2D = GetComponent<CharacterController2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 new_position = new Vector2(x: 0.0f, y: Mathf.Sin(Time.fixedTime)*scale);
+        m_CharacterController2D.Move(new_position);
+
+    }
+}
