ba65fe9a9c050e04bfead04b5c75a3dc7124808e
Eldir <lothvorn@gmail.com>
2018-10-15 21:07:15 +0100

    basic stuff for monster data screen

---
+++ Assets/zzMyAssets/Scripts/MonsterData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "__MonsterData__", menuName = "LoS/Create Monster Data", order = 1)]
+public class MonsterData : ScriptableObject {
+
+    public string m_name;
+    public string m_description;
+    public Sprite m_image;
+}
---
+++ Assets/zzMyAssets/Scripts/MonsterDataController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterDataController : MonoBehaviour {
+
+
+    public void BTN_Next ()
+    {
+
+    }
+
+    public void BTN_Prev ()
+    {
+
+    }
+
+    public void BH_DisplayForList ()
+    {
+
+        m_btnNext.gameObject.SetActive(false);
+        m_btnPrev.gameObject.SetActive(false);
+
+        gameObject.SetActive(true);
+    }
+
+    public void BH_DisplayForSingle ()
+    {
+
+    }
+
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    [SerializeField] Button m_btnNext;
+    [SerializeField] Button m_btnPrev;
+
+
+}
